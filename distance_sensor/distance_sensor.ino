13ed46317ada16955d9b3b1e4401d21e082ab333
DoritosLover1 <ihsandmrci10@gmail.com>
2025-09-26 19:05:22 +0300

    Added LED Screen.

--- distance_sensor/distance_sensor.ino
+++ distance_sensor/distance_sensor.ino
@@ -1,7 +1,29 @@
 #define TRIG_PIN 2
 #define ECHO_PIN 3
-#define LED_PIN 4
-#define SAFE_LED_PIN 5
+#define LED_PIN 4        
+#define SAFE_LED_PIN 5   
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+unsigned long previousMillis = 0;
+unsigned long ledInterval = 500; 
+bool ledState = LOW;
+
+long distanceCalculator() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  
+  return pulseIn(ECHO_PIN, HIGH) * 0.0343 / 2;
+}
 
 void setup() {
   Serial.begin(9600);
@@ -9,37 +31,60 @@ void setup() {
   pinMode(SAFE_LED_PIN, OUTPUT);
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 bulunamadi"));
+    for (;;);
+  }
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
 }
 
 void loop() {
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
+  long distance = distanceCalculator();
+  Serial.print("Distance: ");
+  Serial.println(distance);
 
-  long distance = pulseIn(ECHO_PIN, HIGH) * 0.0343 / 2;
+  display.clearDisplay();
+  display.setCursor(0,10);
+  display.print("Distance: ");
+  display.print(distance);
+  display.println(" cm");
 
-  if (distance < 10)
-  {
+  String durum;
+  if(distance < 10){
+    durum = "ERROR!";
+    ledInterval = 100; 
     digitalWrite(SAFE_LED_PIN, LOW);
-    digitalWrite(LED_PIN, HIGH);
-    delay(50);
-    digitalWrite(LED_PIN, LOW);
-    delay(50);
   } 
-  else if (distance < 30)
-  {
+  else if(distance < 20){
+    durum = "TOO CLOSE";
+    ledInterval = 300;
     digitalWrite(SAFE_LED_PIN, LOW);
-    digitalWrite(LED_PIN, HIGH);
-    delay(100);
-    digitalWrite(LED_PIN, LOW);
-    delay(100);
   }
-  else
-  {
+  else if(distance < 30){
+    durum = "CLOSE";
+    ledInterval = 500;
+    digitalWrite(SAFE_LED_PIN, LOW);
+  }
+  else {
+    durum = "SAFE";
     digitalWrite(LED_PIN, LOW);
     digitalWrite(SAFE_LED_PIN, HIGH);
+    ledState = LOW; 
   }
-  delay(120);
+
+  display.setCursor(0,30);
+  display.println(durum);
+  display.display();
+
+  if(distance < 30){
+    unsigned long currentMillis = millis();
+    if(currentMillis - previousMillis >= ledInterval){
+      previousMillis = currentMillis;
+      ledState = !ledState;
+      digitalWrite(LED_PIN, ledState);
+    }
+  }
 }
